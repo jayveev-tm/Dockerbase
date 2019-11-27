f253e7a5c682b4af77a755e435e56c051120d4cd
Jayvee Velez <jayvee_velez@trendmicro.com>
2019-11-27 03:50:26 +0000

    Add new file

---
+++ docker.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#install curl
+apt-get update
+apt-get install curl -y
+
+#install docker
+apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt-get update
+apt-get install -y docker-ce=18.06.2~ce~3-0~ubuntu
+cat > /etc/docker/daemon.json <<EOF
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2"
+}
+EOF
+systemctl enable docker.service
+systemctl daemon-reload
+systemctl restart docker.service
+docker run hello-world
